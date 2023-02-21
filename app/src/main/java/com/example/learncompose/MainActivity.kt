12b5fd55ed89aaaa5d97965c5d173bcb64c306c6
Ankit-jailwal <jailwalankit@gmail.com>
2023-02-22 05:28:48 +0530

    Added toolbar and floating action button in Scaffold

--- app/src/main/java/com/example/learncompose/MainActivity.kt
+++ app/src/main/java/com/example/learncompose/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.learncompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,12 +21,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Learn Compose")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick =  { Log.d("msg","Menu Button")}) {
+                                    Icon(Icons.Filled.Menu, contentDescription = "menu")
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick =  { Log.d("msg","Notification Button")}) {
+                                    Icon(Icons.Filled.Notifications, contentDescription = "notification")
+                                }
+                                IconButton(onClick =  { Log.d("msg","Search Button")}) {
+                                    Icon(Icons.Filled.Search, contentDescription = "search")
+                                }
+                            }
+                        )
+                    },
+                    
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {Log.d("msg", "Floating Button")}) {
+                            IconButton(onClick = {}) {
+                                Icon(Icons.Filled.Add, contentDescription = "floatingButton")
+                            }
+                        }
+                    },
+                
                 ) {
-                    Greeting("Android")
+                    padding -> Column(modifier = Modifier.padding(padding)) {
+                    Greeting(name = "Android!")
+                }
                 }
             }
         }
@@ -34,6 +67,11 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun Hey(name: String) {
+    Text(text = "Ankit $name")
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -41,3 +79,4 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
